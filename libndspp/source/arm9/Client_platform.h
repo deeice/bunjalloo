a88757dae4dcff5956f8d35c9ca3fe0fcd3ffd5d
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2007-08-24 21:47:29 +0000

    Remove unused macros
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@242 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- libndspp/source/arm9/Client_platform.h
+++ libndspp/source/arm9/Client_platform.h
@@ -19,8 +19,6 @@
 #define Client_platform_h_seen
 #include "nds.h"
 #define TIMEOUT 2
-#define READ_WAIT_SEC 0
-#define READ_WAIT_USEC 500000
 #define closesocket_platform closesocket
 #define socklen_t int
 #endif
--- libndspp/source/pc/Client_platform.h
+++ libndspp/source/pc/Client_platform.h
@@ -20,7 +20,5 @@
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
 #define TIMEOUT 1
-#define READ_WAIT_SEC 1
-#define READ_WAIT_USEC 0
 #define closesocket_platform close
 #endif
