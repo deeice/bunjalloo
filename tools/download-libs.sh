a770342dda9a4259d6a3ec5dddf264b344fb0c4b
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2007-09-07 16:46:58 +0000

    Helper script for downloading + compiling zlib and libpng
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@291 89bda9af-7d2e-0410-a434-4dd01d8ab487

---
+++ tools/download-libs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+LIBPNG=libpng-1.2.19
+ZLIB=zlib-1.2.3
+
+if [ ! -e ${LIBPNG}.tar.bz2 ] ; then wget ftp://ftp.simplesystems.org/pub/libpng/png/src/${LIBPNG}.tar.bz2 ; fi
+if [ ! -e ${ZLIB}.tar.gz ] ; then wget http://www.zlib.net/${ZLIB}.tar.gz ; fi
+tar xjvf ${LIBPNG}.tar.bz2
+tar xzvf ${ZLIB}.tar.gz
+ln -s ${ZLIB} zlib
+ln -s ${LIBPNG} libpng
+wget http://quirkysoft.googlecode.com/svn/external/zlib/Makefile.ds -O zlib/Makefile.ds
+wget http://quirkysoft.googlecode.com/svn/external/libpng/Makefile.ds -O libpng/Makefile.ds
+make -C zlib -f Makefile.ds
+make -C libpng -f Makefile.ds
