9d99ab6a5f2f8cce0145d6832434503260892008
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2007-09-12 21:29:20 +0000

    Create source package and upload that too.
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@311 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- bunjalloo/makedist.sh
+++ bunjalloo/makedist.sh
@@ -7,6 +7,7 @@ trunk=https://quirkysoft.googlecode.com/svn/trunk
 tags=https://quirkysoft.googlecode.com/svn/tags
 project=bunjalloo
 
+$SVN up
 modifications=$($SVN status -q | wc -l)
 if [ $modifications != 0 ] ; then
   echo "Modified files exist.. commit first before tagging stuff!"
@@ -21,4 +22,9 @@ revision=$(svn info | grep ^Revision | cut -d: -f2 | tr -d ' ')
 make dist
 $MV bunjalloo.zip bunjalloo-${release}.zip
 $UPLOAD -s "$summary" -p quirkysoft -l Type-Archive,Program-Bunjalloo,OpSys-NDS bunjalloo-${release}.zip
-$SVN cp $trunk $tags/${project}_${release}_r${revision}
+tagname=${project}_${release}_r${revision}
+$SVN cp $trunk $tags/$tagname
+
+make dist-src REP=${tags/https/http}/$tagname
+$MV bunjalloo-dist.tar.gz bunjalloo-src-${release}.tar.gz
+$UPLOAD -s "Source code for release $release" -p quirkysoft -l Type-Source,Program-Bunjalloo bunjalloo-src-${release}.tar.gz
