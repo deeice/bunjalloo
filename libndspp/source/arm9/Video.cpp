fd8149487ffa75247a5826a2598a84fa807edfb3
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2007-10-25 22:51:26 +0000

    Fix stupid VRAM_CR pointer use.
    This awful code was causing a crash on r21.
    
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@458 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- libndspp/source/arm9/Video.cpp
+++ libndspp/source/arm9/Video.cpp
@@ -69,13 +69,15 @@ void Video::blend(BLDMOD_MODE_t mode, int first, int second)
 void Video::clear()
 {
   // clear vram 
-  unsigned int * vram = (unsigned int*)VRAM_CR;
+  /*
+  vuint32 * vram = &VRAM_CR;
   for (int i = 0; i < 0x3100/8; ++i) {
     *vram++ = 0;
     *vram++ = 0;
     *vram++ = 0;
     *vram++ = 0;
   }
+  */
 
   for (int i = 0; i < 256; ++i) {
     BG_PALETTE[i] = 0;
