e42a7b0bf2529a1a3acb838c0f14f2ce46289b1a
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2007-11-07 21:57:59 +0000

    Update download script for new libs and externals.
    This script downloads the required libraries, unpacks, builds and
    installs them.
    
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@531 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- tools/download-libs.sh
+++ tools/download-libs.sh
@@ -2,14 +2,55 @@
 
 LIBPNG=libpng-1.2.22
 ZLIB=zlib-1.2.3
+GIFLIB=giflib-4.1.4
 
-if [ ! -e ${LIBPNG}.tar.bz2 ] ; then wget ftp://ftp.simplesystems.org/pub/libpng/png/src/${LIBPNG}.tar.bz2 ; fi
-if [ ! -e ${ZLIB}.tar.gz ] ; then wget http://www.zlib.net/${ZLIB}.tar.gz ; fi
-tar xjvf ${LIBPNG}.tar.bz2
-tar xzvf ${ZLIB}.tar.gz
-ln -s ${ZLIB} zlib
-ln -s ${LIBPNG} libpng
-wget http://quirkysoft.googlecode.com/svn/external/zlib/Makefile.ds -O zlib/Makefile.ds
-wget http://quirkysoft.googlecode.com/svn/external/libpng/Makefile.ds -O libpng/Makefile.ds
-make -C zlib -f Makefile.ds
-make -C libpng -f Makefile.ds
+die() {
+  echo >&2 "$@"
+  exit 1
+}
+
+if [ ! -e ${LIBPNG}.tar.bz2 ] ; then
+  wget ftp://ftp.simplesystems.org/pub/libpng/png/src/${LIBPNG}.tar.bz2 || \
+  die "Unable to download ${LIBPNG}"
+fi
+if [ ! -e ${ZLIB}.tar.gz ] ; then
+  wget http://www.zlib.net/${ZLIB}.tar.gz || \
+  die "Unable to download ${ZLIB}"
+fi
+if [ ! -e ${GIFLIB}.tar.gz ] ; then
+  wget http://downloads.sourceforge.net/libungif/${GIFLIB}.tar.gz  || \
+  die "Unable to download ${GIFLIB}"
+fi
+
+echo "Extracting downloaded libraries..."
+tar xjf ${LIBPNG}.tar.bz2 || die "Problems unpacking ${LIBPNG}"
+tar xzf ${ZLIB}.tar.gz    || die "Problems unpacking ${ZLIB}"
+tar xzf ${GIFLIB}.tar.gz  || die "Problems unpacking ${GIFLIB}"
+
+ln -sf ${ZLIB} zlib
+ln -sf ${LIBPNG} libpng
+ln -sf ${GIFLIB} giflib
+
+echo "Checking out more libraries..."
+svn co http://quirkysoft.googlecode.com/svn/external/ otherlibs || die "Unable to checkout from subversion"
+cp otherlibs/zlib/Makefile.ds       zlib/Makefile.ds
+cp otherlibs/libpng/Makefile.ds     libpng/Makefile.ds
+cp otherlibs/giflib/Makefile.ds     giflib/Makefile.ds
+cp otherlibs/giflib/config.h        giflib/config.h
+cp otherlibs/giflib/lib/Makefile.ds giflib/lib/Makefile.ds
+
+echo "Building and installing the libraries..."
+make -C zlib -f Makefile.ds install || die "Problems building zlib"
+make -C libpng -f Makefile.ds install || die "Problems building libpng"
+make -C giflib -f Makefile.ds install || die "Problems building giflib"
+make -C otherlibs/jpgd -f Makefile.ds install || die "Problems building jpgd"
+make -C otherlibs/matrixssl/src -f Makefile.ds install || die "Problems building matrixssl"
+
+echo
+echo
+echo "You should also run the following to uninstall the dependencies:"
+echo "  make -C zlib -f Makefile.ds uninstall"
+echo "  make -C libpng -f Makefile.ds uninstall"
+echo "  make -C giflib -f Makefile.ds uninstall"
+echo "  make -C otherlibs/jpgd -f Makefile.ds uninstall"
+echo "  make -C otherlibs/matrixssl/src -f Makefile.ds uninstall"
