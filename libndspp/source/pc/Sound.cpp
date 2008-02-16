5e1d18261f30a1bd42dfac00d7ebf19f2467771d
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2008-02-16 18:43:54 +0000

    Update copyright year.
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@631 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- libndspp/source/pc/Sound.cpp
+++ libndspp/source/pc/Sound.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2007 Richard Quirk
+  Copyright (C) 2007,2008 Richard Quirk
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
