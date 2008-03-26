8f3fc90ded38062cae3e9da13a6e44f529a55698
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2008-03-26 21:32:11 +0000

    Refactor a bit.
    Make sure /usr/include does not sneek in.
    
    
    git-svn-id: https://quirkysoft.googlecode.com/svn/branches/topics/wafify@759 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- libndspp/waf_tools/arm.py
+++ libndspp/waf_tools/arm.py
@@ -14,28 +14,29 @@ def setup_tool(conf, proc):
 
   dka_bin='%s/bin'%os.environ[DEVKITARM]
   arm_eabi='arm-eabi-'
-  gcc = conf.find_program(arm_eabi+'gcc', path_list=[dka_bin])
-  gxx = conf.find_program(arm_eabi+'g++', path_list=[dka_bin])
-  if not gcc:
-    conf.fatal('gcc was not found')
-  if not gxx:
-    conf.fatal('gxx was not found')
 
-  v['CC'] = gcc
-  v['CXX'] = gxx
-  v['AR'] = '%sar'%arm_eabi
-  v['RANLIB'] = '%sranlib'%arm_eabi
+  # reset the tools
+  for i in ( ('gcc','CC'), ('g++','CXX'), ('ar','AR'), ('ranlib','RANLIB')):
+    if v[i[1]]:
+      continue
+    t = conf.find_program(arm_eabi+i[0], path_list=[dka_bin])
+    if not t:
+      conf.fatal(i[0]+' was not found')
+    v[i[1]] = t
+
   v['LINK_CC'] = v['CC']
   v['CCFLAGS_ARM%d'%proc] = (ARCH+' -g -ffast-math').split()
   v['CCFLAGS_ARM%d'%proc].extend({9:' -march=armv5te -mtune=arm946e-s -DARM9',
       7:' -mcpu=arm7tdmi -mtune=arm7tdmi -DARM7 '}[proc].split())
   v['CPPFLAGS_ARM%d'%proc] = ' -g -fno-rtti -fno-exceptions '.split()
-  v['CPPPATH'] = ['%s/libnds/include'%os.environ[DEVKITPRO]]
+  path = ['%s/libnds/include'%os.environ[DEVKITPRO]]
+  #v['CPPPATH'] = path
+  v['CPPPATH_ARM%d'%proc] = path
   v['LINKFLAGS_ARM%d'%proc] = ['-L%s/libnds/lib'%os.environ[DEVKITPRO], '-lnds%d'%proc, '-specs=ds_arm%d.specs'%proc,
       '-g','-mno-fpu','-Wl,-Map,map%d.map'%proc, '-Wl,-gc-sections']
   v['LIB_ARM%d'%proc] = ['nds%d'%proc]
   v['LINK_CXX'] = v['CXX']
-  v['CXXPATH'] = v['CPPPATH']
+  #v['CXXPATH'] = v['CPPPATH']
   v['CPPFLAGS_ARM%d'%proc].extend(v['CCFLAGS_ARM%d'%proc])
   v['CXXDEFINES'] = v['CCDEFINES']
 
