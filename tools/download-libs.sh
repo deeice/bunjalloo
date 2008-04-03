993be427c34ea1378cedb535bdae9dc19eb0c250
richard.quirk <richard.quirk@89bda9af-7d2e-0410-a434-4dd01d8ab487>
2008-04-03 21:54:11 +0000

    Fix download-libs.sh.
    Update to latest libpng.
    Minor fixes so that the script does what it should.
    Patch jpeglib's jmorecfg to remove floating points.
    More instructions.
    
    
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@792 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- tools/download-libs.sh
+++ tools/download-libs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LIBPNG=libpng-1.2.22
+LIBPNG=libpng-1.2.26
 ZLIB=zlib-1.2.3
 GIFLIB=giflib-4.1.6
 JPEGLIB=jpegsrc.v6b
@@ -23,7 +23,7 @@ if [ ! -e ${GIFLIB}.tar.gz ] ; then
   die "Unable to download ${GIFLIB}"
 fi
 if [ ! -e ${JPEGLIB}.tar.gz ] ; then
-  wget http://quirkysoft.googlecode.com/files/${JPEGLIB}.tar.gz
+  wget http://quirkysoft.googlecode.com/files/${JPEGLIB}.tar.gz || \
   die "Unable to download ${JPEGLIB}"
 fi
 
@@ -41,32 +41,40 @@ ln -sf jpeg-6b jpeglib
 echo "Checking out more libraries..."
 svn co http://quirkysoft.googlecode.com/svn/external/ otherlibs || die "Unable to checkout from subversion"
 cp otherlibs/zlib/Makefile.ds       zlib/Makefile.ds
-cp otherlibs/zlib/contrib/minizip/Makefile.{ds,linux}  zlib/contrib/minizip/
+cp otherlibs/zlib/contrib/minizip/Makefile.* zlib/contrib/minizip/
 cp otherlibs/libpng/Makefile.ds     libpng/Makefile.ds
 cp otherlibs/giflib/Makefile.ds     giflib/Makefile.ds
 cp otherlibs/giflib/config.h        giflib/config.h
 cp otherlibs/giflib/lib/Makefile.ds giflib/lib/Makefile.ds
 
 echo "Building and installing the libraries..."
-make -C zlib -f Makefile.ds install || die "Problems building zlib"
-make -C zlib/contrib/minizip -f Makefile.ds install || die "Problems building unzip lib"
+make -C zlib -f Makefile.ds clean install || die "Problems building zlib"
+make -C zlib/contrib/minizip -f Makefile.ds clean install || die "Problems building unzip lib"
 echo "Also needed for SDL build:"
 echo "sudo make -C zlib/contrib/minizip -f Makefile.linux clean install"
-make -C libpng -f Makefile.ds install || die "Problems building libpng"
-make -C giflib -f Makefile.ds install || die "Problems building giflib"
+make -C libpng -f Makefile.ds clean install || die "Problems building libpng"
+make -C giflib -f Makefile.ds clean install || die "Problems building giflib"
+make -C otherlibs/matrixssl/src -f Makefile.ds clean install || die "Problems building matrixSSL"
 cd jpeglib
+patch -i ../otherlibs/jpeglib/jmorecfg-remove-float.patch -p1 || die "Unable to patch JPEG to remove floats"
 CFLAGS="-Dsprintf=siprintf -Dfprintf=fiprintf -DNO_GETENV -O2 -march=armv5te -mtune=arm946e-s -ffast-math" \
  LDFLAGS="-L$DEVKITPRO/libnds/lib -lnds9 -specs=ds_arm9.specs" CC=$DEVKITARM/bin/arm-eabi-gcc ./configure --prefix=$DEVKITPRO/libnds || die "Problems configuring jpeg lib"
-make
+make clean
 make install-lib
 cd -
 
 echo
 echo
-echo "You should run the following to uninstall the dependencies:"
+echo "You can run the following to uninstall the dependencies:"
 echo "  make -C zlib -f Makefile.ds uninstall"
+echo "  make -C zlib/contrib/minizip -f Makefile.ds uninstall"
 echo "  make -C libpng -f Makefile.ds uninstall"
 echo "  make -C giflib -f Makefile.ds uninstall"
 echo "  make -C jpeglib uninstall-lib"
 echo "  make -C otherlibs/matrixssl/src -f Makefile.ds uninstall"
-
+echo
+echo "Additionally, you will need these libraries for the Linux/SDL test build:"
+echo "zlib libpng unzip giflib jpeg matrixssl"
+echo "Most are standard, except for unzip and matrixssl. Try running:"
+echo "  make -C zlib/contrib/minizip -f Makefile.linux clean install"
+echo "  make -C otherlibs/matrixssl/src -f Makefile clean install"
