8558a3c7cea3c843ddeec2f0a805805485c55da4
Richard Quirk <richard.quirk@gmail.com>
2008-04-13 20:28:52 +0000

    Port the unit test code from the waf branch.
    This makes unit test results quieter, unless there is a failure.
    
    
    git-svn-id: https://quirkysoft.googlecode.com/svn/trunk@821 89bda9af-7d2e-0410-a434-4dd01d8ab487

--- libndspp/ndspp-scons/unit_test.py
+++ libndspp/ndspp-scons/unit_test.py
@@ -16,7 +16,6 @@
 """
 """  SCons tool specification for unit tests """
 
-import os, os.path
 from SCons.Builder import Builder
 
 def exists(env):
@@ -26,18 +25,23 @@ def generate(env, **kw):
   def run_unit_test(target, source, env):
     """ Execute unit tests, creating a test.passed cache file. Based on
      www.scons.org/wiki/UnitTest modified to run tests the source dir. """
+    import os, os.path, subprocess
     dirname = str(source[0].srcnode().dir)
     app = str(source[0].abspath)
     cwd = os.getcwd()
-    print "scons: Entering directory `%s'"%os.path.join(cwd,dirname)
     os.chdir(dirname)
-    result = os.spawnl(os.P_WAIT, app, app)
+    pp = subprocess.Popen(app, stdout=subprocess.PIPE)
+    pp.wait()
+    if pp.returncode:
+      # this sorts out jump-to-errors in e.g. vim
+      print "scons: Entering directory `%s'"%os.path.join(cwd,dirname)
+      print pp.stdout.read()
     os.chdir(cwd)
-    if result == 0:
-      open(str(target[0]),'w').write("PASSED\n")
-      return 0
-    else:
-      return 1
+    out = 'FAILED'
+    if pp.returncode == 0:
+      out = 'PASSED'
+    open(str(target[0]),'w').write(out+"\n")
+    return pp.returncode
   # Create a builder for tests
   bld = Builder(action = run_unit_test)
   env.Append(BUILDERS = {'Test' :  bld})
