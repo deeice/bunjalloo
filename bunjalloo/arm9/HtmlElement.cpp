eef2d7f1bead3377a5bca019cedcf69c4093471f
Richard Quirk <richard.quirk@gmail.com>
2008-04-21 21:17:27 +0200

    Include <algorithm> for find, for_each, transform.

--- bunjalloo/arm9/HtmlElement.cpp
+++ bunjalloo/arm9/HtmlElement.cpp
@@ -15,6 +15,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <vector>
+#include <algorithm>
 #include "ElementFactory.h"
 #include "HtmlConstants.h"
 #include "HtmlElement.h"
