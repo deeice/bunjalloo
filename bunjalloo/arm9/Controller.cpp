8992bcc60accc732b808d401eaa234b04e443d92
Richard Quirk <richard.quirk@gmail.com>
2008-04-21 21:15:27 +0200

    cstring now required for memcpy, strlen, etc.

--- bunjalloo/arm9/Controller.cpp
+++ bunjalloo/arm9/Controller.cpp
@@ -14,8 +14,9 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "libnds.h"
 #include <vector>
+#include <cstring>
+#include "libnds.h"
 #include "Cache.h"
 #include "Config.h"
 #include "ConfigParser.h"
